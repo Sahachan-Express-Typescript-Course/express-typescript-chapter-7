752886ea083728f4df6c95ebb56888714ffb5211
oat431 <oat431@gmail.com>
2025-01-18 12:02:54 +0700

    chore: setup chapter 7

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import { AppDataSource } from './data-source.js';
+import { MovieReloadData } from './config/reload.movie.js';
 import express, { urlencoded, json } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+
+// feature
+import movieRouter from './movie/router/movie.router.js';
 
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || 'localhost';
 const app = express();
+const movieReloadData = new MovieReloadData();
 
 app.use(json());
+app.use(cors());
 app.use(urlencoded({ extended: true }));
 app.use(morgan(':date[iso] | :method | :url | :status | :res[content-length] - :response-time ms'));
 
@@ -15,9 +22,13 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
 
+// movie route
+app.use('/api/v1/movie', movieRouter);
+
 AppDataSource.initialize()
     .then(async () => {
         console.log('Database connected');
+        await movieReloadData.loadData();
         app.listen(PORT, () => {
             console.log(`Server running at http://${HOST}:${PORT}`);
         });
---
+++ src/movie/router/movie.router.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { MovieController } from '../controller/movie.controller.js';
+
+const movieRouter = Router();
+const movieController = new MovieController();
+
+// movie feature
+movieRouter.get('/', movieController.getAllMovies);
+movieRouter.get('/:id', movieController.getMovieById);
+movieRouter.post('/', movieController.createMovie);
+movieRouter.put('/:id', movieController.updateMovie);
+movieRouter.delete('/:id', movieController.deleteMovie);
+
+// comment feature
+movieRouter.post('/:id/comment', movieController.addComment);
+movieRouter.get('/:id/comment/:comment_id', movieController.getCommentById);
+movieRouter.put('/:id/comment/:comment_id', movieController.updateComment);
+movieRouter.delete('/:id/comment/:comment_id', movieController.deleteComment);
+
+// advance feature
+movieRouter.get('/:id/avg', movieController.getAvgRating);
+movieRouter.get('/summary/comment', movieController.getTotalCommentFromEachMovie);
+movieRouter.get('/summary/rating', movieController.getMovieRateSummary);
+movieRouter.get('/summary/search', movieController.searchMovie);
+
+export default movieRouter;
